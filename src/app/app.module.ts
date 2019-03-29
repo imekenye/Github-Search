b8ee0db4f5f44ef44d0c9e2ba107e3da93567b46
imekenye <ismekenye@gmail.com>
2019-03-29 13:15:13 -0700

    Import modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsideComponent } from './aside/aside.component';
+import { HeaderComponent } from './header/header.component';
+import { RepoCardComponent } from './repo-card/repo-card.component';
+import { RepoCardDetailsComponent } from './repo-card-details/repo-card-details.component';
+import { MainAreaComponent } from './main-area/main-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    HeaderComponent,
+    RepoCardComponent,
+    RepoCardDetailsComponent,
+    MainAreaComponent
   ],
   imports: [
     BrowserModule,
