4473391737decb8da1fc78c72b56e036ec1a5059
imekenye <ismekenye@gmail.com>
2019-03-31 11:48:39 -0700

    Update header,repo-card and user details

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "src/app/services/data.service";
+import { Users } from "../../user";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  users: any = [Users];
+  username: string;
+  repos: any = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
+  searchUser() {
+    this.dataService.updateUsers(this.username);
+    this.dataService.getUsers().subscribe(users => {
+      console.log(users);
+      this.users = users;
+    });
 
-  ngOnInit() {
+    this.dataService.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
   }
 
+  ngOnInit() {}
 }
