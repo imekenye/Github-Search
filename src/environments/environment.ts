f434924fcaefd66eb592f877f6cac4cff064c13e
imekenye <ismekenye@gmail.com>
2019-03-31 09:05:40 -0700

    Add access token to environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "https://api.github.com/",
+  accessToken: "7699d4f052332db3b555a2061ccafd973059d563"
 };
 
 /*
