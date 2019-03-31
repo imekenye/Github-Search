989bfe637c80aa0b78000bd7a9f6e99dae1bd8e2
imekenye <ismekenye@gmail.com>
2019-03-31 11:47:36 -0700

    Update aside components details

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Users } from "../../user";
+import { DataService } from "../../services/data.service";
 
 @Component({
-  selector: 'app-aside',
-  templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.scss']
+  selector: "app-aside",
+  templateUrl: "./aside.component.html",
+  styleUrls: ["./aside.component.scss"]
 })
 export class AsideComponent implements OnInit {
+  users: any = Users;
+  repos: any = [];
+  created_at: any;
+  username: string;
 
-  constructor() { }
+  constructor(private dataService: DataService) {
+    this.users = new Users("", "", "", "", 0, "", new Date());
+    this.dataService.getUsers().subscribe(users => {
+      console.log(users);
+      this.users = users;
+    });
 
-  ngOnInit() {
+    this.dataService.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
   }
 
+  ngOnInit() {}
 }
