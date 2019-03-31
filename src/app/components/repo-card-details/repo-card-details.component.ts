a3fe82d7f5c6be9ecc58a2d75b44189d00e5d848
imekenye <ismekenye@gmail.com>
2019-03-31 11:47:09 -0700

    Update repo card details

--- src/app/components/repo-card-details/repo-card-details.component.ts
+++ src/app/components/repo-card-details/repo-card-details.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { DataService } from "../../services/data.service";
+import { Repos } from "../../repos";
 
 @Component({
-  selector: 'app-repo-card-details',
-  templateUrl: './repo-card-details.component.html',
-  styleUrls: ['./repo-card-details.component.scss']
+  selector: "app-repo-card-details",
+  templateUrl: "./repo-card-details.component.html",
+  styleUrls: ["./repo-card-details.component.scss"]
 })
 export class RepoCardDetailsComponent implements OnInit {
+  repos: any = [Repos];
+  reponame: string;
+  show: number;
 
-  constructor() { }
+  constructor(private dataService: DataService) {
+    this.repos = new Repos("", "", new Date(), "");
+    this.dataService.getRepoInfo().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
 
-  ngOnInit() {
+    this.dataService.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
   }
 
+  ngOnInit() {}
 }
